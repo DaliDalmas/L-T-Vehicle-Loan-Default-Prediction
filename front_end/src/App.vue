94209c78a7f3f59d2bc18337925f86cc19e7a34a
Dalmas.Otieno <dalmas.otieno@GLB-1602019-1779.local>
2022-12-03 03:08:45 +0300

    front end start

---
+++ front_end/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#114856"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="logo.png"
+          transition="scale-transition"
+          width="60"
+        />
+
+        DALICODES' LOAN DETECTION SYSTEM
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://www.youtube.com/channel/UCzSlSeJ4XH4bWH79DKmIxjg"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Subscribe here</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ front_end/src/components/HelloWorld.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+       
+      </v-col>
+
+      <v-col class="mb-4" color="#114856">
+        <h1 class="display-2 font-weight-bold mb-3 text_primary">
+          VEHICLE LOAN DETECTION SYSTEM
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Answer the questions below to determine if your client is eligible for vehicle loan.
+        </p>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>
+
+<style scoped>
+.text_primary{
+  color: #114856;
+}
+</style>
